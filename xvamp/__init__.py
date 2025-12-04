1b68b5c8c6b0a6a3dfcb5cbeb246ebe11fe7ac43
Tobias Köhne <tobias.koehne@dlr.de>
2025-12-04 09:52:36 +0100

    add Np/km unit and equivalencies

--- xvamp/__init__.py
+++ xvamp/__init__.py
@@ -29,12 +29,31 @@ DBKM_CM_FACTOR = np.log(10) / 1e6
 DB_CM_EQ = [
     (u.dB / u.km, u.cm**-1, lambda x: x * DBKM_CM_FACTOR, lambda x: x / DBKM_CM_FACTOR)
 ]
-# add unit and equivalences to global scope
+# define the Neper unit
+NP_UNIT = u.def_unit("Np")
+# add the absorption coefficient equivalences between Np and dB and 1/cm
+DB_NP_FACTOR = np.log10(np.e) / 20
+CM_DB_FACTOR = DB_NP_FACTOR / DBKM_CM_FACTOR
+DBKM_CM_NPKM_EQ = [
+    (
+        u.dB / u.km,
+        NP_UNIT / u.km,
+        lambda x: x * DB_NP_FACTOR,
+        lambda x: x / DB_NP_FACTOR,
+    ),
+    (
+        u.cm**-1,
+        NP_UNIT / u.km,
+        lambda x: x * CM_DB_FACTOR,
+        lambda x: x / CM_DB_FACTOR,
+    ),
+]
+# add units and equivalences to global scope
 # also add "ppm" and "atm" as units necessary for files or equations
 try:
-    u.add_enabled_units([N_UNIT, cds.ppm, cds.atm])
+    u.add_enabled_units([N_UNIT, NP_UNIT, cds.ppm, cds.atm])
 except ValueError as e:  # already enabled
     if "Object with name 'Nunit' already exists in namespace" not in str(e):
         raise
 else:
-    u.add_enabled_equivalencies(N_UNIT_EQ + DB_CM_EQ)
+    u.add_enabled_equivalencies(N_UNIT_EQ + DB_CM_EQ + DBKM_CM_NPKM_EQ)
