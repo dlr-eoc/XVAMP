3714aa92c8356e4f106bb3235178af0c1199b144
Tobias Köhne <tobias.koehne@dlr.de>
2026-07-02 16:09:36 +0200

    provide H2SO4 profiles in KS98

--- xvamp/references/kolodner_steffes_1998.py
+++ xvamp/references/kolodner_steffes_1998.py
@@ -8,12 +8,11 @@ import astropy.units as u
 from importlib.resources import files as res_files
 from typing import Tuple
 from astropy.units import Quantity
-from astropy.table import QTable, hstack
 
 # package imports
 from .. import data
 from ..constants import ESU_CM, SPEC_MOL_M
-from ..utils import read_unit_csv
+from ..utils import read_unit_csv, Profile
 
 # data location
 BASEFOLDER = "kolodner_steffes_1998"
@@ -36,20 +35,11 @@ T_H2SO4 = Quantity(553, "K")
 # others only defined in _get_eps_prime_r_and_molar_density
 
 
+# define function to compute the real part of the relative permittivity and the
+# molar density of H2SO4 in the experiment as described in Section 3.2.
 def _get_eps_prime_r_and_molar_density() -> (
     Tuple[Quantity["dimensionless"], Quantity["molar concentration"]]
 ):
-    """
-    Computes the real part of the relative permittivity and the molar density
-    of H2SO4 in the experiment as described in Section 3.2.
-
-    Returns
-    -------
-    eps_prime_r
-        Real part of the relative permittivity of H2SO4 in the experiment
-    rho
-        Molar density of H2SO4 in the experiment
-    """
     # refractivity of the gaseous sulfuric acid
     n_h2so4 = Quantity((340.64 + 245.36) / 2, "Nunit")
     # mass density of the sulfuric acid solution before it evaporates
@@ -73,20 +63,42 @@ def _get_eps_prime_r_and_molar_density() -> (
 
 
 # evaluate the experiment
-eps_prime_r_h2so4, rho_h2so4 = _get_eps_prime_r_and_molar_density()
+_eps_mdens = _get_eps_prime_r_and_molar_density()
+eps_prime_r_h2so4 = _eps_mdens[0]
+""" Real part of the relative permittivity of H2SO4 in the experiment """
+rho_h2so4 = _eps_mdens[1]
+""" Molar density of H2SO4 in the experiment """
+
+# define quick access to each of the three X-band orbital data as Profiles,
+# clipped to non-negative values
+h2so4_mr_3212 = Profile(
+    index=tables["fig789"]["altitude"],
+    data=tables["fig789"]["3212"].value.clip(0),
+    data_unit=tables["fig789"]["3212"].unit,
+)
+""" H2SO4 mixing ratio from Magellan orbit 3212 """
+h2so4_mr_3213 = Profile(
+    index=tables["fig789"]["altitude"],
+    data=tables["fig789"]["3213"].value.clip(0),
+    data_unit=tables["fig789"]["3213"].unit,
+)
+""" H2SO4 mixing ratio from Magellan orbit 3213 """
+h2so4_mr_3214 = Profile(
+    index=tables["fig789"]["altitude"],
+    data=tables["fig789"]["3214"].value.clip(0),
+    data_unit=tables["fig789"]["3214"].unit,
+)
+""" H2SO4 mixing ratio from Magellan orbit 3214 """
 
 # average the three different orbits to have a single H2SO4 abundance
 # profile from X-band
-tables["H2SO4 X-band"] = hstack(
-    [
-        tables["fig789"]["altitude"],
-        QTable.from_pandas(
-            tables["fig789"]
-            .to_pandas(index="altitude")
-            .mean(axis=1)
-            .clip(lower=0)
-            .to_frame("mixing ratio of H2SO4"),
-            units={"mixing ratio of H2SO4": tables["fig789"]["3212"].unit},
-        ),
-    ]
+h2so4_mr_mean = Profile(
+    index=tables["fig789"]["altitude"],
+    data=tables["fig789"]
+    .to_pandas(index="altitude")
+    .mean(axis=1)
+    .clip(lower=0)
+    .to_numpy(),
+    data_unit=tables["fig789"]["3212"].unit,
 )
+""" Mean H2SO4 mixing ratio from Magellan orbits 3212, 3213 and 3214 """
